3819365ff5a607949d37c45a892692bd3fc69e8f
jin <103798287+jinftw64@users.noreply.github.com>
2024-06-07 15:05:06 -0400

    start adding weather elements

--- src/dom.js
+++ src/dom.js
@@ -2,6 +2,7 @@ import config from './config.js';
 import weather from './weather.js';
 
 import formTemplate from './templates/form.html';
+import locationBar from './templates/location-bar.html'
 import './style.css';
 
 const dom = (() => {
@@ -17,7 +18,6 @@ const dom = (() => {
   }
 
   const eventHandlers = () => {
-    const submit = document.querySelector('#search');
     const renderedForm = document.querySelector('form.zip-code-form');
     const zipField = document.querySelector('#zip');
     const unitOfMeasurement = document.querySelector('input[name="unit-of-measurement"]:checked');
@@ -28,14 +28,36 @@ const dom = (() => {
       config.zipCode = zipField.value;
       config.unitOfMeasurement = unitOfMeasurement.value;
 
-      console.log(await weather.getWeather());
+      const weatherData = await weather.getWeather();
+
+      resetDOM();
+      renderLocationBar(weatherData);
     })
   }
 
   const resetDOM = () => {
-    while (document.body.firstChild) {
-      document.body.removeChild(document.body.lastChild);
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
     }
+  }
+
+  const renderLocationBar = (someObject) => {
+    const locationDiv = document.createElement('div');
+    const city = someObject.name;
+    const region = someObject.region;
+
+    locationDiv.innerHTML = locationBar;
+
+    container.appendChild(locationDiv);
+
+    const locationInput = document.querySelector('#location');
+
+    locationInput.setAttribute('onfocus', 'this.value=""');
+    locationInput.value = `${city}, ${region}`;
+  }
+
+  const renderNow = () => {
+
   }
 
   return {
