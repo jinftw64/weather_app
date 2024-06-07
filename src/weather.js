4028fd26a4678c3f814eb9e4273db2124a73e310
jin <103798287+jinftw64@users.noreply.github.com>
2024-06-07 13:24:57 -0400

    add new function to parse current weather data

--- src/weather.js
+++ src/weather.js
@@ -2,12 +2,34 @@ import config from "./config";
 
 const weather = (() => {
   const getResponseJson = async () => {
-    const currentURL = `https://api.weatherapi.com/v1/forecast.json?key=${config.apiKey}&q=${config.zipCode}&days=${config.daysOfForecast}`;
+    const currentURL = `https://api.weatherapi.com/v1/forecast.json?`
+      + `key=${config.apiKey}`
+      + `&q=${config.zipCode}`
+      + `&days=${config.daysOfForecast}`;
+
     const response = await fetch(currentURL);
     const weatherData = await response.json();
 
     console.log(weatherData);
     return weatherData;
+  }
+
+  // current day requires different deserialization vs forecast day
+  const createNow = (someObject) => {
+    const now = {
+      date: someObject.current.last_updated,
+      temp_c: someObject.current.temp_c,
+      temp_f: someObject.current.temp_f,
+      feelslike_c: someObject.current.feelslike_c,
+      feelslike_f: someObject.current.feelslike_f,
+      maxtemp_c: someObject.forecast.forecastday[0].day.maxtemp_c,
+      maxtemp_f: someObject.forecast.forecastday[0].day.maxtemp_f,
+      mintemp_c: someObject.forecast.forecastday[0].day.mintemp_c,
+      mintemp_f: someObject.forecast.forecastday[0].day.mintemp_f,
+      condition: someObject.current.condition.text,
+    }
+
+    return now;
   }
 
   const createDay = (someObject) => {
@@ -32,7 +54,12 @@ const weather = (() => {
     const week = {};
 
     for (const dayNum in forecastday) {
-      week[dayNum] = createDay(forecastday[dayNum]);
+      // create today object if index is 0 otherwise create forecast day
+      if (dayNum == '0') {
+        week[dayNum] = createNow(someJson);
+      } else {
+        week[dayNum] = createDay(forecastday[dayNum]);
+      }
     }
 
     return {
