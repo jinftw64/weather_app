b59b60c1fd77a7314202271b3d678b26ce38d613
jin <103798287+jinftw64@users.noreply.github.com>
2024-06-11 13:01:21 -0400

    create map of codes and image paths

--- src/imageMap.js
+++ src/imageMap.js
@@ -10,4 +10,531 @@ const imageMap = {
     }
   },
 
+  1003: {
+    day: {
+      icon: './icons/partly_cloudy_day.svg.svg',
+      banner_folder: './banners/03-partly-cloudy-day/'
+    },
+    night: {
+      icon: './icons/partly_cloudy_night.svg',
+      banner_folder: './banners/07-partly-cloudy-night/'
+    }
+  },
+
+  1006: {
+    day: {
+      icon: './icons/cloudy.svg',
+      banner_folder: './banners/09-cloudy/'
+    },
+    night: {
+      icon: './icons/mostly_cloudy_night.svg',
+      banner_folder: './banners/08-mostly-cloudy-night/'
+    }
+  },
+
+  1009: {
+    day: {
+      icon: './icons/mostly_cloudy_day.svg',
+      banner_folder: './banners/09-cloudy/'
+    },
+    night: {
+      icon: './icons/mostly_cloudy_night.svg',
+      banner_folder: './banners/08-mostly-cloudy-night/'
+    }
+  },
+
+  1030: {
+    day: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/26-haze-fog-dust-smoke/'
+    },
+    night: {
+      icon: './icons/haze_fog_dust_smoke.svg',
+      banner_folder: './banners/26-haze-fog-dust-smoke/'
+    }
+  },
+
+  1063: {
+    day: {
+      icon: './icons/sunny_with_rain.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/scattered_showers_night.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1066: {
+    day: {
+      icon: './icons/scattered_snow_showers_day.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    }
+  },
+
+  1069: {
+    day: {
+      icon: './icons/sleet_hail.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/sleet_hail.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1072: {
+    day: {
+      icon: './icons/sleet_hail.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/sleet_hail.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1087: {
+    day: {
+      icon: './icons/isolated_scattered_thunderstorms_day.svg',
+      banner_folder: './banners/22-iso-thunderstorms/'
+    },
+    night: {
+      icon: './icons/isolated_scattered_thunderstorms_night.svg',
+      banner_folder: './banners/22-iso-thunderstorms/'
+    }
+  },
+
+  1114: {
+    day: {
+      icon: './icons/blowing_snow.svg',
+      banner_folder: './banners/16-blowing-snow/'
+    },
+    night: {
+      icon: './icons/blowing_snow.svg',
+      banner_folder: './banners/16-blowing-snow/'
+    }
+  },
+
+  1117: {
+    day: {
+      icon: './icons/blizzard.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    },
+    night: {
+      icon: './icons/blizzard.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    }
+  },
+
+  1117: {
+    day: {
+      icon: './icons/blizzard.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    },
+    night: {
+      icon: './icons/blizzard.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    }
+  },
+
+  1135: {
+    day: {
+      icon: './icons/haze_fog_dust_smoke.svg',
+      banner_folder: './banners/26-haze-fog-dust-smoke/'
+    },
+    night: {
+      icon: './icons/haze_fog_dust_smoke.svg',
+      banner_folder: './banners/26-haze-fog-dust-smoke/'
+    }
+  },
+
+  1147: {
+    day: {
+      icon: './icons/haze_fog_dust_smoke.svg',
+      banner_folder: './banners/26-haze-fog-dust-smoke/'
+    },
+    night: {
+      icon: './icons/haze_fog_dust_smoke.svg',
+      banner_folder: './banners/26-haze-fog-dust-smoke/'
+    }
+  },
+
+  1150: {
+    day: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1153: {
+    day: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1168: {
+    day: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1171: {
+    day: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1180: {
+    day: {
+      icon: './icons/scattered_showers_day.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/scattered_showers_night.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1183: {
+    day: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    },
+    night: {
+      icon: './icons/drizzle.svg',
+      banner_folder: './banners/10-drizzle/'
+    }
+  },
+
+  1186: {
+    day: {
+      icon: './icons/sunny_with_rain.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/sunny_with_rain.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1189: {
+    day: {
+      icon: './icons/sunny_with_rain.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/sunny_with_rain.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1192: {
+    day: {
+      icon: './icons/heavy_rain.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/heavy_rain.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1195: {
+    day: {
+      icon: './icons/heavy_rain.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/heavy_rain.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1198: {
+    day: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1201: {
+    day: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1204: {
+    day: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1207: {
+    day: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1210: {
+    day: {
+      icon: './icons/scattered_snow_showers_day.svg',
+      banner_folder: './banners/13-flurries/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1213: {
+    day: {
+      icon: './icons/flurries.svg',
+      banner_folder: './banners/13-flurries/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/13-flurries/'
+    }
+  },
+
+  1216: {
+    day: {
+      icon: './icons/flurries.svg',
+      banner_folder: './banners/13-flurries/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/13-flurries/'
+    }
+  },
+
+  1219: {
+    day: {
+      icon: './icons/blowing_snow.svg',
+      banner_folder: './banners/16-blowing-snow/'
+    },
+    night: {
+      icon: './icons/blowing_snow.svg',
+      banner_folder: './banners/16-blowing-snow/'
+    }
+  },
+
+  1222: {
+    day: {
+      icon: './icons/blowing_snow.svg',
+      banner_folder: './banners/16-blowing-snow/'
+    },
+    night: {
+      icon: './icons/blowing_snow.svg',
+      banner_folder: './banners/16-blowing-snow/'
+    }
+  },
+
+  1225: {
+    day: {
+      icon: './icons/heavy_snow.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    },
+    night: {
+      icon: './icons/heavy_snow.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    }
+  },
+
+  1237: {
+    day: {
+      icon: './icons/heavy_snow.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    },
+    night: {
+      icon: './icons/heavy_snow.svg',
+      banner_folder: './banners/17-heavy-snow-blizzard/'
+    }
+  },
+
+  1240: {
+    day: {
+      icon: './icons/scattered_showers_day.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/scattered_showers_night.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1243: {
+    day: {
+      icon: './icons/scattered_showers_day.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/scattered_showers_night.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1246: {
+    day: {
+      icon: './icons/heavy_rain.svg',
+      banner_folder: './banners/11-rain/'
+    },
+    night: {
+      icon: './icons/heavy_rain.svg',
+      banner_folder: './banners/11-rain/'
+    }
+  },
+
+  1249: {
+    day: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1252: {
+    day: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/mixed_rain_snow.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1255: {
+    day: {
+      icon: './icons/scattered_snow_showers_day.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    }
+  },
+
+  1258: {
+    day: {
+      icon: './icons/scattered_snow_showers_day.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    }
+  },
+
+  1261: {
+    day: {
+      icon: './icons/scattered_snow_showers_day.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    },
+    night: {
+      icon: './icons/scattered_snow_showers_night.svg',
+      banner_folder: './banners/15-snow-showers-snow/'
+    }
+  },
+
+  1264: {
+    day: {
+      icon: './icons/sleet_hail.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    },
+    night: {
+      icon: './icons/sleet_hail.svg',
+      banner_folder: './banners/19-mixed-rain-hail-rain-sleet/'
+    }
+  },
+
+  1273: {
+    day: {
+      icon: './icons/isolated_scattered_thunderstorms_day.svg',
+      banner_folder: './banners/23-scattered-thunderstorms/'
+    },
+    night: {
+      icon: './icons/isolated_scattered_thunderstorms_night.svg',
+      banner_folder: './banners/23-scattered-thunderstorms/'
+    }
+  },
+
+  1276: {
+    day: {
+      icon: './icons/strong_thunderstorms.svg',
+      banner_folder: './banners/24-strong-thunderstorms/'
+    },
+    night: {
+      icon: './icons/strong_thunderstorms.svg',
+      banner_folder: './banners/24-strong-thunderstorms/'
+    }
+  },
+
+  1279: {
+    day: {
+      icon: './icons/strong_thunderstorms.svg',
+      banner_folder: './banners/24-strong-thunderstorms/'
+    },
+    night: {
+      icon: './icons/strong_thunderstorms.svg',
+      banner_folder: './banners/24-strong-thunderstorms/'
+    }
+  },
+
+  1282: {
+    day: {
+      icon: './icons/strong_thunderstorms.svg',
+      banner_folder: './banners/24-strong-thunderstorms/'
+    },
+    night: {
+      icon: './icons/strong_thunderstorms.svg',
+      banner_folder: './banners/24-strong-thunderstorms/'
+    }
+  },
 }
